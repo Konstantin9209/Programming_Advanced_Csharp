ccf0e61a4e9e441af63bce03fda38a4b00c92895
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-07 23:56:43 +0000

    Updated repeat string program

--- 02.RepeatStrings/Program.cs
+++ 02.RepeatStrings/Program.cs
@@ -1,2 +1,13 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string[] words = Console.ReadLine().Split(" ");
+
+string output = "";
+
+for (int i = 0; i < words.Length; i++)
+{
+    string currentWord = words[i];
+    for (int j = 0; j < currentWord.Length; j++)
+    {
+        output += currentWord;
+    }
+}
+Console.WriteLine(output);
