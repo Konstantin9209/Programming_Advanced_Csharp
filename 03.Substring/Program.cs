7927290f1b1b4daea1f208a5f79ce4b9cf3feaa9
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-08 11:52:55 +0000

    Adding task 3 substring

--- 03.Substring/Program.cs
+++ 03.Substring/Program.cs
@@ -1,2 +1,12 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//string fruits = "banana, apple";
+//Console.WriteLine(fruits.IndexOf("apple"));
+
+string wordToRemove = Console.ReadLine();
+string text = Console.ReadLine();
+
+while (text.Contains(wordToRemove))
+{
+    text = text.Replace(wordToRemove, "");
+
+}
+Console.WriteLine(text);
