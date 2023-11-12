43541f18f61b1bb3b1baff9ab6404c333e05a254
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-12 09:04:26 +0000

    Adding Emailvalidator tests #10

--- 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/EmailValidatorTests.cs
+++ 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/EmailValidatorTests.cs
@@ -4,10 +4,10 @@ namespace TestApp.UnitTests;
 
 public class EmailValidatorTests
 {
-    // TODO: finish the test
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
+
+    [TestCase("ivan@abv.bg")]
+    [TestCase("petko@abv.bg")]
+    [TestCase("azsym@abv.bg")]
     public void Test_ValidEmails_ReturnsTrue(string email)
     {
         // Arrange
@@ -19,10 +19,10 @@ public class EmailValidatorTests
         Assert.That(result, Is.True);
     }
 
-    // TODO: finish the test
-    //[TestCase()]
-    //[TestCase()]
-    //[TestCase()]
+   
+    [TestCase("invalid email@abv.bg")]
+    [TestCase("validemail@abv_avb.bg")]
+    [TestCase("invalid/email@abv.bg")]
     public void Test_InvalidEmails_ReturnsFalse(string email)
     {
         // Arrange
