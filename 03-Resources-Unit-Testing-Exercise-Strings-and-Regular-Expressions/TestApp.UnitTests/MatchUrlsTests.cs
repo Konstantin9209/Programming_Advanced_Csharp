a0ae873592249fbd63e0847ab7c06c0a6764855a
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-12 08:57:18 +0000

    Adding Match URL Tests #9

--- 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/MatchUrlsTests.cs
+++ 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/MatchUrlsTests.cs
@@ -6,45 +6,62 @@ namespace TestApp.UnitTests;
 
 public class MatchUrlsTests
 {
-    // TODO: finish the test
+   
     [Test]
     public void Test_ExtractUrls_EmptyText_ReturnsEmptyList()
     {
         // Arrange
         string text = "";
+        List<string> expected = new();
 
         // Act
-
+        List<string> result =MatchUrls.ExtractUrls(text);
         // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     // TODO: finish the test
     [Test]
     public void Test_ExtractUrls_NoUrlsInText_ReturnsEmptyList()
     {
-        // Arrange
+        string text = "No valid URLs";
 
         // Act
-
+        List<string> result = MatchUrls.ExtractUrls(text);
         // Assert
-        //Assert.That(result, Is.Empty);
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
     public void Test_ExtractUrls_SingleUrlInText_ReturnsSingleUrl()
     {
-        // TODO: finish the test
+        string text = "Single URL: https://softuni.bg";
+        List<string> expected = new() { "https://softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractUrls_MultipleUrlsInText_ReturnsAllUrls()
     {
-        // TODO: finish the test
+        string text = "Multiple URL: https://www.softuni.bg, https://zpetkov.com, https://qualitysolutionslab.zpetkov.com";
+        List<string> expected = new() { "https://www.softuni.bg", "https://zpetkov.com", "https://qualitysolutionslab.zpetkov.com" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractUrls_UrlsInQuotationMarks_ReturnsUrlsInQuotationMarks()
     {
-        // TODO: finish the test
+        string text = "Single URL: \"https://softuni.bg/about\"";
+        List<string> expected = new() { "https://softuni.bg" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
