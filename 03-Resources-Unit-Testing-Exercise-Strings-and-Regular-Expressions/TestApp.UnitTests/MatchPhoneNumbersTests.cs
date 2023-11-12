5f529a353359d2c825d8a471129af71b71e38830
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-12 08:23:25 +0000

    Adding Matched Phone numbers tests #7

--- 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/MatchPhoneNumbersTests.cs
+++ 03-Resources-Unit-Testing-Exercise-Strings-and-Regular-Expressions/TestApp.UnitTests/MatchPhoneNumbersTests.cs
@@ -10,28 +10,43 @@ public class MatchPhoneNumbersTests
     {
         // Arrange
         string phoneNumbers = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555";
-
+        string expected = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555";
         // Act
-
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_NoValidPhoneNumbers_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        string phoneNumbers = "+39-2-124-5678, + 2 986 5432, +359-2-555-555";
+        string expected = "";
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_EmptyInput_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        string phoneNumbers = "";
+        string expected = "";
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Match_MixedValidAndInvalidNumbers_ReturnsOnlyValidNumbers()
     {
-        // TODO: finish the test
+        string phoneNumbers = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555, +39-2-124-5678, + 2 986 5432, +359-2-555-555";
+        string expected = "+359-2-124-5678, +359 2 986 5432, +359-2-555-5555";
+        // Act
+        string result = MatchPhoneNumbers.Match(phoneNumbers);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
