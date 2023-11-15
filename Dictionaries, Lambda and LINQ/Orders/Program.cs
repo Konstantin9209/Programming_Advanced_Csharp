fd5ebbdd9964ce2eff4007bf68ab909686401976
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-15 15:18:05 +0000

    Adding task 3 orders

--- Dictionaries, Lambda and LINQ/Orders/Program.cs
+++ Dictionaries, Lambda and LINQ/Orders/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary < string, List < decimal >> products = new();
+
+string input = Console.ReadLine();
+
+while (input != "buy")
+{
+    string[] inputArray = input.Split();
+    string product = inputArray[0];
+    decimal price = decimal.Parse(inputArray[1]);
+    decimal quantity = decimal.Parse(inputArray[2]);
+
+    if (!products.ContainsKey(product))
+    {
+        products.Add(product, new List<decimal>());
+        product.[product].Add(price);
+        product.[product].Add(quantity);
+    
+    }
+    else
+    {
+        products[product][0] = price;
+        products[product][1] = quantity;
+    }
+}
