8a203523b12a661df8ae56ecfc657c1f6d4543e3
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-15 14:54:28 +0000

    Updating task 2 Miner task

--- Dictionaries, Lambda and LINQ/A Miner Task/Program.cs
+++ Dictionaries, Lambda and LINQ/A Miner Task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, int> resources = new();
+
+string resource = Console.ReadLine();
+
+while (resource != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (!resources.ContainsKey(resource))
+    {
+        resources.Add(resource, quantity);
+    }
+    else
+    {
+        resources[resource] += quantity;
+    }
+
+    resource = Console.ReadLine();
+}
+
+foreach (KeyValuePair<string, int> pair in resources)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
--- Dictionaries, Lambda and LINQ/Count Chars in a String/Program.cs
+++ Dictionaries, Lambda and LINQ/Count Chars in a String/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<char, int> characters = new();
+
+string input = Console.ReadLine();
+
+foreach (char ch in input)
+{
+    if (ch ==  ' ')
+    {
+        continue;
+    }
+    if (characters.ContainsKey(ch))
+    {
+        characters[ch]++;
+    }
+    else
+    {
+        characters.Add(ch, 1); 
+    }
+}
+
+foreach (KeyValuePair<char, int> pair in characters)
+{
+    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+}
