14b853936929ac44d4ce203751a6a2ce6e42bce6
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-17 22:14:17 +0000

    Adding Unit tests for Count Characters

--- Dictionaries Unit Testing/TestApp.Tests/CountCharactersTests.cs
+++ Dictionaries Unit Testing/TestApp.Tests/CountCharactersTests.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace TestApp.Tests;
 
@@ -24,29 +25,63 @@ public class CountCharactersTests
     [Test]
     public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
     {
-        // Arrange
+        List<string> input = new() { "", "", "" };
 
         // Act
+        string result = CountCharacters.Count(input);
 
         // Assert
-        //Assert.That(result, Is.Empty);
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
     public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
     {
-        // TODO: finish test
+        List<string> input = new() { "aaa", "aa", "a" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("a -> 6"));
     }
 
     [Test]
     public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
     {
-        // TODO: finish test
+        List<string> input = new() { "aaa", "aabbccc", "ababcba" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 8");
+        sb.AppendLine("b -> 5");
+        sb.AppendLine("c -> 4");
+        string expected = sb.ToString().Trim();
+
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
     {
-        // TODO: finish test
+        List<string> input = new() { "aaa!", "aab!bccc", "ababc!ba" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 8");
+        sb.AppendLine("! -> 3");
+        sb.AppendLine("b -> 5");
+        sb.AppendLine("c -> 4");
+        string expected = sb.ToString().Trim();
+
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
