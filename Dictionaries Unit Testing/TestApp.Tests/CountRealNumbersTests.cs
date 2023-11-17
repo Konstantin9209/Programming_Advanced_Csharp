7705c871494567ab263d7d12caf1cf68f7b5c277
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-17 22:39:55 +0000

    Adding unit tests for  count real numbers

--- Dictionaries Unit Testing/TestApp.Tests/CountRealNumbersTests.cs
+++ Dictionaries Unit Testing/TestApp.Tests/CountRealNumbersTests.cs
@@ -6,39 +6,55 @@ namespace TestApp.Tests;
 
 public class CountRealNumbersTests
 {
-    // TODO: finish test
+
     [Test]
     public void Test_Count_WithEmptyArray_ShouldReturnEmptyString()
     {
         // Arrange
-
+        int[] input = new int[0];
         // Act
-
+        string result = CountRealNumbers.Count(input);
         // Assert
-        //Assert.That(result, Is.Empty);
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
     public void Test_Count_WithSingleNumber_ShouldReturnCountString()
     {
-        // TODO: finish test
+        int[] input = new int[] { 1 };
+        // Act
+        string result = CountRealNumbers.Count(input);
+        // Assert
+        Assert.That(result, Is.EqualTo("1 -> 1"));
     }
 
     [Test]
     public void Test_Count_WithMultipleNumbers_ShouldReturnCountString()
     {
-        // TODO: finish test
+        int[] input = new int[] { 1, 2, 3, 1, 2, 1, 3, 2 };
+        // Act
+        string result = CountRealNumbers.Count(input);
+        // Assert
+        Assert.That(result, Is.EqualTo("1 -> 3\r\n2 -> 3\r\n3 -> 2"));
     }
 
     [Test]
     public void Test_Count_WithNegativeNumbers_ShouldReturnCountString()
     {
-        // TODO: finish test
+        int[] input = new int[] { -1, -2, -3, -1, -2, -1, -3, -2 };
+        // Act
+        string result = CountRealNumbers.Count(input);
+        // Assert
+        Assert.That(result, Is.EqualTo("-3 -> 2\r\n-2 -> 3\r\n-1 -> 3"));
     }
 
     [Test]
     public void Test_Count_WithZero_ShouldReturnCountString()
     {
-        // TODO: finish test
+        int[] input = new int[] { 0, 0, 0, 0, 0 };
+        // Act
+        string result = CountRealNumbers.Count(input);
+        // Assert
+        Assert.That(result, Is.EqualTo("0 -> 5"));
     }
 }
