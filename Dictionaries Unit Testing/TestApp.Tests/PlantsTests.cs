c29fe09b780810a03667aae91c82034946dd8a8f
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-18 00:25:46 +0000

    Adding tests for Plants tests task

--- Dictionaries Unit Testing/TestApp.Tests/PlantsTests.cs
+++ Dictionaries Unit Testing/TestApp.Tests/PlantsTests.cs
@@ -9,30 +9,61 @@ public class PlantsTests
     [Test]
     public void Test_GetFastestGrowing_WithEmptyArray_ShouldReturnEmptyString()
     {
-        // TODO: finish test
+        // Arrange
+        string[] plants = new string[0];
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.Empty);
     }
 
-    // TODO: finish test
+
     [Test]
     public void Test_GetFastestGrowing_WithSinglePlant_ShouldReturnPlant()
     {
-        // Arrange
-
+        string[] plants = new string[]
+        {
+            "@--\\-"
+        };
         // Act
-        //string result = Plants.GetFastestGrowing(plants);
+        string result = Plants.GetFastestGrowing(plants);
 
         // Assert
+        Assert.That(result, Is.EqualTo("Plants with 5 letters:\r\n@--\\-"));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMultiplePlants_ShouldReturnGroupedPlants()
     {
-        // TODO: finish test
+        string[] plants = new string[]
+ {
+            "@--\\-",
+            "*---",
+            "----",
+            "*-\\-\\"
+ };
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\n*---\r\n----\r\nPlants with 5 letters:\r\n@--\\-\r\n*-\\-\\"));
     }
 
     [Test]
     public void Test_GetFastestGrowing_WithMixedCasePlants_ShouldBeCaseInsensitive()
     {
-        // TODO: finish test
+        string[] plants = new string[]
+{
+            "rOse",
+            "liLy",
+            "aBcde",
+            "Fddgr"
+};
+        // Act
+        string result = Plants.GetFastestGrowing(plants);
+
+        // Assert
+        Assert.That(result, Is.EqualTo("Plants with 4 letters:\r\nrOse\r\nliLy\r\nPlants with 5 letters:\r\naBcde\r\nFddgr"));
     }
 }
