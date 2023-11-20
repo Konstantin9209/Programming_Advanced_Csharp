d242e1794a13acf23080a9d36dfcfecf2f0fc8c9
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-20 22:57:58 +0000

    Add task 2 students

---
+++ Objects and Classes/Students/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Students
+{
+    public class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            firstName = firstName;
+            lastName = lastName;
+            age = age;
+            homeTown = homeTown;
+
+        public string FirstName = { get; set; }
+    
