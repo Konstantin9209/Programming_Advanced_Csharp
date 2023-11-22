78555c6333085315a300c9bb6b984683e9e7b668
Konstantin9209 <konstantin.z.petkov.@gmail.com>
2023-11-22 23:19:48 +0000

    Updating Car Manufacturer with more data #1

--- Objects and Classes/Car Manufacturer/Car.cs
+++ Objects and Classes/Car Manufacturer/Car.cs
@@ -12,5 +12,24 @@ namespace CarManufacturer
        public string Model { get; set; }
 
         public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - distance * this.FuelConsumption > 0)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return ($"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuelQuantity: {this.FuelQuantity:F2}");
+        }
     }
 }
