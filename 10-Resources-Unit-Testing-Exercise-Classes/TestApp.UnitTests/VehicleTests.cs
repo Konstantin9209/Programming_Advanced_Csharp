bf34f644ce9ecba928680d54a8115d5542cf9450
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-11-29 23:51:15 +0000

    Add task vehicles tests

--- 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/VehicleTests.cs
+++ 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/VehicleTests.cs
@@ -8,25 +8,35 @@ namespace TestApp.UnitTests;
 
 public class VehicleTests
 {
-    // TODO: write the setup method
 
-    // TODO: finish test
     [Test]
     public void Test_AddAndGetCatalogue_ReturnsSortedCatalogue()
-    {
+    { 
+   
+        Vehicles vehicles = new Vehicles();
+        string[] input =
+        {
+            "{Car/Ford/Focus/120",
+            "Car/Toyota/Camry/150",
+            "Truck/Volvo/VNL/500"
+        };
         // Arrange
         string expected = $"Cars:{Environment.NewLine}Ford: Focus - 120hp{Environment.NewLine}Toyota: Camry - 150hp{Environment.NewLine}Trucks:{Environment.NewLine}Volvo: VNL - 500kg";
 
         // Act
-        //string result = this._vehicles.AddAndGetCatalogue(input);
+        string result = vehicles.AddAndGetCatalogue(input);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_AddAndGetCatalogue_ReturnsEmptyCatalogue_WhenNoDataGiven()
     {
-        // TODO: finish test
+        Vehicles vehicles1 = new Vehicles();
+        string[] input = { };
+        string expected = $"Cars:{Environment.NewLine}Trucks:";
+        string result = vehicles1.AddAndGetCatalogue(input);
+        Assert.AreEqual(expected, result);
     }
 }
