a7536118a20bce046e61be11f01a22752208af2a
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-11-30 14:46:41 +0000

    Adding tests for task 7 planet tests

--- 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/PlanetTests.cs
+++ 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/PlanetTests.cs
@@ -1,28 +1,36 @@
 ﻿using NUnit.Framework;
-
 using System;
 
-namespace TestApp.UnitTests;
-
-public class PlanetTests
+namespace TestApp.UnitTests
 {
-    // TODO: finish test
-    [Test]
-    public void Test_CalculateGravity_ReturnsCorrectCalculation()
+    public class PlanetTests
     {
-        // Arrange
-        //Planet earth = new("Earth", 12742, 149600000, 1);
-        double mass = 1000;
-        //double expectedGravity = mass * 6.67430e-11 / Math.Pow(earth.Diameter / 2, 2);
+        [Test]
+        public void Test_CalculateGravity_ReturnsCorrectCalculation()
+        {
+            Planet earth = new("Earth", 12742, 149600000, 1);
+            double mass = 1000;
+            double expectedGravity = mass * 6.67430e-11 / Math.Pow(earth.Diameter / 2, 2);
 
-        // Act
+            double result = earth.CalculateGravity(mass);
 
-        // Assert
-    }
+            Assert.AreEqual(expectedGravity, result);
+        }
 
-    [Test]
-    public void Test_GetPlanetInfo_ReturnsCorrectInfo()
-    {
-        // TODO: finish test
+        [Test]
+        public void Test_GetPlanetInfo_ReturnsCorrectInfo()
+        {
+            // Arrange
+            Planet earth = new("Earth", 12742, 149600000, 1);
+
+            // Act
+
+
+            // Assert
+            string expected = $"Planet: Earth{Environment.NewLine}Diameter: 12742 km{Environment.NewLine}Distance from the Sun: 149600000 km{Environment.NewLine}Number of Moons: 1";
+
+            string actual = earth.GetPlanetInfo();
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
