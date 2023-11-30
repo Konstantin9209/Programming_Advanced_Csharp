b33a1b8200d5cc35cba47507fcd144422c7a9220
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-11-30 23:50:02 +0000

    adding task 1 Square root throw exception

---
+++ Exceptions Throwing/Square root/Program.cs
@@ -0,0 +1,22 @@
+﻿try
+{
+int num = int.Parse(Console.ReadLine());
+    if (num < 0)
+    {
+        throw new Exception();
+    }
+    double sqrt = Math.Sqrt(num);
+    Console.WriteLine(sqrt);
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("Invalid number.");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid format.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
