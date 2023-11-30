5f5d9c6b82390138ff48b0418d2e2dc0f1ff5cf6
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-11-30 14:20:16 +0000

    Add task 6 Article tests

--- 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/ArticleTests.cs
+++ 10-Resources-Unit-Testing-Exercise-Classes/TestApp.UnitTests/ArticleTests.cs
@@ -1,37 +1,87 @@
 ﻿using NUnit.Framework;
-
 using System;
 
-namespace TestApp.UnitTests;
-
-public class ArticleTests
+namespace TestApp.UnitTests
 {
-    // TODO: write the setup method
-
-    // TODO: finish test
-    [Test]
-    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    public class ArticleTests
     {
-        // Arrange
+        private Article _article;
 
-        // Act
+        [SetUp]
+        public void SetUp()
+        {
+            this._article = new Article();
+        }
 
-        // Assert
-        //Assert.That(result.ArticleList, Has.Count.EqualTo(3));
-        //Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
-        //Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
-        //Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
-    }
+        [Test]
+        public void Test_AddArticles_ReturnsArticleWithCorrectData()
+        {
+            // Arrange
+            string[] articleData =
+            {
+                "Article1 Content1 Author1",
+                "Article2 Content2 Author2",
+                "Article3 Content3 Author3",
+            };
 
-    [Test]
-    public void Test_GetArticleList_SortsArticlesByTitle()
-    {
-        // TODO: finish test
-    }
+            // Act
+            Article result = this._article.AddArticles(articleData);
 
-    [Test]
-    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
-    {
-        // TODO: finish test
+            // Assert
+            Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+            Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article1"));
+            Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+            Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+        }
+
+        [Test]
+        public void Test_GetArticleList_SortsArticlesByTitle()
+        {
+            // Arrange
+            Article inputArticle = new Article()
+            {
+                ArticleList = new System.Collections.Generic.List<Article>()
+                {
+                    new Article()
+                    {
+                        Author = "Teodor",
+                        Content = "Some Content",
+                        Title = "Begginers Coding"
+                    },
+                    new Article()
+                    {
+                        Author = "Ivan",
+                        Content = "Other Content",
+                        Title = "A brief overview of Architecture"
+                    }
+                }
+            };
+
+            string expected = $"A brief overview of Architecture - Other Content: Ivan{Environment.NewLine}Begginers Coding - Some Content: Teodor";
+
+            // Act
+            string actual = this._article.GetArticleList(inputArticle, "title");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+        {
+            // Arrange
+            Article inputArticle = new Article()
+            {
+                ArticleList = new System.Collections.Generic.List<Article>()
+            };
+
+            string expected = "";
+
+            // Act
+            string actual = this._article.GetArticleList(inputArticle, "invalidCriteria");
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
