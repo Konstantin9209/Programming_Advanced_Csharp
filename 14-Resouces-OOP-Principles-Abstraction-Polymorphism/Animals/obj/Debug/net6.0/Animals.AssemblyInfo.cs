773151df48faf1be47f26ba4b22d97d0df9aa202
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-12-11 01:04:04 +0000

    Add task 4 Heroes

--- 14-Resouces-OOP-Principles-Abstraction-Polymorphism/Animals/obj/Debug/net6.0/Animals.AssemblyInfo.cs
+++ 14-Resouces-OOP-Principles-Abstraction-Polymorphism/Animals/obj/Debug/net6.0/Animals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Animals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca47342599d64bbdc630dd45cad81ace9e423a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Animals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Animals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
