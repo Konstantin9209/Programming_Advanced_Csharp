87ae406e2d3a9788c72dbef4c590196d9d90f60a
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-12-11 23:17:24 +0000

    Adding task 2 from the exam prep

--- Exam Preparation/02-Dictionary-Insertion-Resources/02-Dictionary-Insertion-Resources/TestApp.Tests/DictionaryIntersectionTests.cs
+++ Exam Preparation/02-Dictionary-Insertion-Resources/02-Dictionary-Insertion-Resources/TestApp.Tests/DictionaryIntersectionTests.cs
@@ -10,30 +10,78 @@ public class DictionaryIntersectionTests
     [Test]
     public void Test_Intersect_TwoEmptyDictionaries_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        Dictionary<string, int> firstDictonary = new Dictionary<string, int>();
+        Dictionary<string, int> secondDictonary = new();
+
+        Dictionary<string , int> result = DictionaryIntersection.Intersect(firstDictonary, secondDictonary);
+        Assert.AreEqual(0, result.Count);
     }
 
     [Test]
     public void Test_Intersect_OneEmptyDictionaryAndOneNonEmptyDictionary_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        Dictionary<string, int> firstDictonary = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+        };
+        Dictionary<string, int> secondDictonary = new();
+
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictonary, secondDictonary);
+        Assert.AreEqual(0, result.Count);
     }
 
     [Test]
     public void Test_Intersect_TwoNonEmptyDictionariesWithNoCommonKeys_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        Dictionary<string, int> firstDictonary = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+        };
+        Dictionary<string, int> secondDictonary = new()
+                    {
+            { "three", 3 },
+            { "four", 4 },
+        };
+
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictonary, secondDictonary);
+        Assert.AreEqual(0, result.Count);
     }
 
     [Test]
     public void Test_Intersect_TwoNonEmptyDictionariesWithCommonKeysAndValues_ReturnsIntersectionDictionary()
     {
-        // TODO: finish this test
+        Dictionary<string, int> firstDictonary = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+        };
+        Dictionary<string, int> secondDictonary = new()
+                    {
+            { "one", 1 },
+            { "four", 4 },
+        };
+
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictonary, secondDictonary);
+        Assert.AreEqual(1, result.Count);
     }
 
     [Test]
     public void Test_Intersect_TwoNonEmptyDictionariesWithCommonKeysAndDifferentValues_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        Dictionary<string, int> firstDictonary = new()
+        {
+            { "one", 1 },
+            { "two", 2 },
+        };
+        Dictionary<string, int> secondDictonary = new()
+                    {
+            { "one", 10 },
+            { "two", 20 },
+        };
+
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictonary, secondDictonary);
+        Assert.AreEqual(0, result.Count);
     }
 }
