ade83489cd59038683f7784974ac016d8b91aa2f
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-12-12 00:20:40 +0000

    Adding task 3 product - exam prep

--- Exam Preparation/03-Product-Resources/ClassesResources/TestApp.Tests/ProductInventoryTests.cs
+++ Exam Preparation/03-Product-Resources/ClassesResources/TestApp.Tests/ProductInventoryTests.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using System;
 using TestApp.Product;
 
 namespace TestApp.Tests;
@@ -18,30 +18,73 @@ public class ProductInventoryTests
     [Test]
     public void Test_AddProduct_ProductAddedToInventory()
     {
-        // TODO: finish the test
+        string productName = "Banana";
+        double productPrice = 100;
+        int productQuantity = 10;
+
+        string expectedInventory = $"Product Inventory:{Environment.NewLine}{productName} - Price: ${productPrice:f2} - Quantity: {productQuantity}";
+
+        this._inventory.AddProduct(productName, productPrice, productQuantity);
+        string result = this._inventory.DisplayInventory();
+
+
+
+        Assert.AreEqual(expectedInventory, result);
     }
 
     [Test]
     public void Test_DisplayInventory_NoProducts_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        string expected = "Product Inventory:";
+
+        string result = this._inventory.DisplayInventory();
+
+        Assert.AreEqual (expected, result);
     }
 
     [Test]
     public void Test_DisplayInventory_WithProducts_ReturnsFormattedInventory()
     {
-        // TODO: finish the test
+        string firstProductName = "Tuna";
+        double firstProductPrice = 100;
+        int firstProductQuantity = 10;
+
+        string secondProductName = "Rice";
+        double secondProductPrice = 10;
+        int secondProductQuantity = 5;
+
+        string expectedOutput = $"Product Inventory:{Environment.NewLine}{firstProductName} - Price: ${firstProductPrice:f2} - Quantity: {firstProductQuantity}{Environment.NewLine}{secondProductName} - Price: ${secondProductPrice:f2} - Quantity: {secondProductQuantity}";
+   this._inventory.AddProduct(firstProductName, firstProductPrice, firstProductQuantity);
+        this._inventory.AddProduct(secondProductName, secondProductPrice, secondProductQuantity);
+
+        string result = this._inventory.DisplayInventory();
+
+        Assert.AreEqual(expectedOutput, result);
     }
 
     [Test]
     public void Test_CalculateTotalValue_NoProducts_ReturnsZero()
     {
-        // TODO: finish the test
+      double result = this._inventory.CalculateTotalValue();
+        Assert.AreEqual(0, result);
     }
 
     [Test]
     public void Test_CalculateTotalValue_WithProducts_ReturnsTotalValue()
     {
-        // TODO: finish the test
+        string firstProductName = "Bread";
+        double firstProductPrice = 100;
+        int firstProductQuantity = 2;
+
+        string secondProductName = "Sushi";
+        double secondProductPrice = 10;
+        int secondProductQuantity = 5;
+        double expectedTotalSum = 250;
+
+        this._inventory.AddProduct(firstProductName, firstProductPrice, firstProductQuantity);
+        this._inventory.AddProduct(secondProductName, secondProductPrice, secondProductQuantity);
+
+        double result = this._inventory.CalculateTotalValue();
+        Assert.AreEqual(expectedTotalSum, result);
     }
 }
