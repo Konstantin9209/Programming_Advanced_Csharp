42b22ae078f22ae6b1d2bd6f24e5a9681105bc3f
Konstantin Petkov <133546359+Konstantin9209@users.noreply.github.com>
2023-12-13 22:52:36 +0000

    .

--- Exam prep 3/01. Substring Extractor/TestApp.Tests/obj/Debug/net6.0/TestApp.Tests.AssemblyInfo.cs
+++ Exam prep 3/01. Substring Extractor/TestApp.Tests/obj/Debug/net6.0/TestApp.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f950db4641637b641eb8af86072a8ef353918465")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
